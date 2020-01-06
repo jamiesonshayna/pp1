60ed1a4eced669b57c60634cc211c60fa028bbe8
Shayna Jamieson <sjamieson2@mail.greenriver.edu>
2020-01-06 14:38:27 -0500

    Add index file

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @author Shayna Jamieson
+ * @version 1.0
+ * @date 1/6/2020
+ * @url http://sjamieson.greenriverdev.com/328/pp1/index.php
+ * @filename Pair Program 1
+ */
+
+    // error reporting
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pair Program 1</title>
+</head>
+<body>
+    <h1>Pair Program 1</h1>
+
+</body>
+</html>
